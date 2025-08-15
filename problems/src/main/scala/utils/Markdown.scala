9efb4ebfd541e384ea9815ccdc6bfa966a1f1333
Dominique Unruh <git.assessments.mkuftc@rwth.unruh.de>
2025-08-15 18:26:54 +0200

    Markdown support for tables

--- problems/src/main/scala/utils/Markdown.scala
+++ problems/src/main/scala/utils/Markdown.scala
@@ -1,12 +1,20 @@
 package utils
 
+import org.commonmark.ext.gfm.tables.TablesExtension
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
 
-object Markdown {
-  private val markdownParser = Parser.builder.build
-  private val markdownRenderer = HtmlRenderer.builder.build
+import scala.jdk.CollectionConverters.SeqHasAsJava
 
+object Markdown {
+  private val extensions = Seq(TablesExtension.create()).asJava
+  private val markdownParser = Parser.builder
+    .extensions(extensions)
+    .build
+  private val markdownRenderer = HtmlRenderer.builder
+    .extensions(extensions)
+    .build
+  
   def markdownToHtml(markdown: String): String = {
     val markdownEscaped = markdown.replace("\\", "\\\\") // commonmark parse treats \( as ( etc. So we quote the \. Could be refinded
     val parsed = markdownParser.parse(markdownEscaped)
