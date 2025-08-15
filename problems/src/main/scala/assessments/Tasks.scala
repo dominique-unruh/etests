113b9cbee175b571ef62e2405c190a12f9c5545d
Dominique Unruh <git.assessments.mkuftc@rwth.unruh.de>
2025-08-15 17:38:06 +0200

    In report: producing errors.html

--- problems/src/main/scala/assessments/Tasks.scala
+++ problems/src/main/scala/assessments/Tasks.scala
@@ -13,6 +13,7 @@ import utils.Utils
 
 import java.io.{BufferedWriter, FileWriter, PrintWriter}
 import java.nio.file.{Files, Path, Paths}
+import scala.collection.mutable
 import scala.util.Using
 
 abstract class Task extends App {
@@ -24,11 +25,11 @@ object GradeEveryone extends Task {
   val exam = Iqc1Exam // TODO don't hardcode
   makeReports()
 
-  private def makeQuestionReport(student: String, question: Assessment): (Points, String) = {
+  private def makeQuestionReport(student: String, question: Assessment, errors: mutable.Queue[(String, Assessment, String)]): (Points, String) = {
     given ExceptionContext = initialExceptionContext(s"Creating report for $student, question '${question.name}'")
     val output = new StringBuilder
     var points: Points = 0
-    
+
     output ++= s"<h1>Problem: ${escapeHtml4(question.name)}</h1>\n"
 
     try {
@@ -55,21 +56,25 @@ object GradeEveryone extends Task {
       points += commenter.points
       output ++= "<ul>\n"
       for (comment <- commenter.comments) {
-        val style = comment.kind match
+        if (comment.kind == Kind.warning)
+          errors += ((student, question, s"Warning: ${comment.html}"))
+        val style = comment.kind match {
           case Kind.feedback => ""
           case Kind.warning => "color: red"
           case Kind.debug => "color: gray"
+        }
         output ++= s"""<li style="$style">${comment.html}</li>\n"""
       }
       output ++= "</ul>\n"
     } catch {
       case e: Throwable =>
-        output ++ s"""<pre style="color:red">${escapeHtml4(ExceptionUtils.getStackTrace(e))}</pre>\n"""
+        output ++= s"""<pre style="color:red">${escapeHtml4(ExceptionUtils.getStackTrace(e))}</pre>\n"""
+        errors += ((student, question, s"Exception: <pre>${escapeHtml4(e.getMessage)}</pre>"))
     }
     (points, output.result())
   }
 
-  private def makeReport(student: String, targetDir: Path) = {
+  private def makeReport(student: String, targetDir: Path, errors: mutable.Queue[(String, Assessment, String)]): Unit = {
     val studentDir = targetDir.resolve(student)
     var totalPoints = Points(0)
     Files.createDirectories(studentDir)
@@ -90,7 +95,7 @@ object GradeEveryone extends Task {
         val pdf = Dynexite.getAnswerPDF(registrationNumber = student)
         Files.write(studentDir.resolve("dynexite.pdf"), pdf)
         for (question <- exam.problems) {
-          val (points, report) = makeQuestionReport(student, question)
+          val (points, report) = makeQuestionReport(student, question, errors)
           totalPoints += points
           writer.write(report)
         }
@@ -100,9 +105,24 @@ object GradeEveryone extends Task {
     }
   }
 
+  private def makeErrorReport(errors: mutable.Queue[(String, Assessment, String)], reportPath: Path): Unit = {
+    Using.resource(new PrintWriter(reportPath.toFile)) { writer =>
+      writer.println("<h1>Errors</h1>")
+      writer.println("<ul>")
+      for ((student, question, message) <- errors) {
+        writer.println(s"""<li><a href="$student/grading.html">$student, ${question.name}</a>:""")
+//        writer.println(s"""(<a href="http://localhost:9000/preview/${question.getClass.getName}/">Webapp</a>):""")
+        writer.println(s"""<span style="color:red">$message</span></li>""")
+      }
+      writer.println("</ul>")
+    }
+  }
+  
   private def makeReports(): Unit = {
     val targetDir = Utils.getSystemPropertyPath("private.report.dir", "the directory where to write the private reports")
+    val errors = mutable.Queue[(String, Assessment, String)]()
     for (student <- Dynexite.resultsByLearner.keys)
-      makeReport(student, targetDir)
+      makeReport(student, targetDir, errors)
+    makeErrorReport(errors, targetDir.resolve("errors.html"))
   }
 }
