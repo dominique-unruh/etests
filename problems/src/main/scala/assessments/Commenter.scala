5304b379b5ec43830de22dfde836ad2e73634744
Dominique Unruh <git.assessments.mkuftc@rwth.unruh.de>
2025-08-17 22:54:07 +0200

    Commenter.gradeBlock

--- problems/src/main/scala/assessments/Commenter.scala
+++ problems/src/main/scala/assessments/Commenter.scala
@@ -3,13 +3,17 @@ package assessments
 import assessments.Comment.Format.markdown
 import assessments.Comment.Kind
 import assessments.Comment.Kind.{feedback, warning}
+import assessments.Commenter.Reachable
 import org.apache.commons.text.StringEscapeUtils
 import org.jsoup.Jsoup
 import utils.Markdown
 
 import scala.annotation.targetName
 import scala.collection.mutable
+import scala.util.boundary
+import scala.util.boundary.{Label, break}
 
+/** Object to collect grading comments and points. */
 final class Commenter {
   var points: Points = 0 
   private val builder = Seq.newBuilder[Comment]
@@ -19,6 +23,49 @@ final class Commenter {
   def +=(comment: Comment): Unit = builder += comment
   def comments: Seq[Comment] = builder.result()
   def clear(): Unit = builder.clear()
+  /** Starts a block for grading a single subproblem.
+   *
+   * In the block, you can normally add comments using `+=`.
+   * You can refer to the reachable points of the whole block as [[max]].
+   * You can exit the block with a given number of points using [[done]]`(n)`.
+   * (These will be automatically added to [[points]] and the block will be exited.
+   * An additional comment will be added saying how many points out of how many 
+   * were added.)
+   *
+   * Example:
+   * {{{
+   * commenter.gradeBlock(40) {
+   *   if (everything good)
+   *     commenter += "Well done"
+   *     done(max)
+ *     if (so so)
+   *     commenter += "So so"
+   *     done(max/2)
+   *   commenter += "Sorry"
+   *   done(0)
+   * }
+   * }}}
+   *
+   * @param max Number of reachable points for this subproblem.
+   * @param body A block which does grading for the subproblem
+   * */
+  def gradeBlock(max: Points)(body: Label[Points] ?=> Reachable ?=> Nothing): Unit = {
+    given Reachable = new Reachable(max)
+    val reached = boundary(body)
+    this += s"$reached out of $max points"
+    points += reached
+  }
+}
+
+object Commenter {
+  /** To be used inside [[Commenter.gradeBlock]] */
+  def max(using reachable: Reachable): Points = reachable.points
+  /** To be used inside [[Commenter.gradeBlock]] */
+  def done(points: Points)(using label: Label[Points]): Nothing = {
+    break(points)
+  }
+  /** For internal use by [[Commenter.gradeBlock]] */
+  final class Reachable private[Commenter] (private[Commenter] val points: Points) extends AnyVal
 }
 
 case class Comment(val html: String, val kind: Comment.Kind) {
