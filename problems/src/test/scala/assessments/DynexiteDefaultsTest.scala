662fab55b3f4701a0a4b0910ca120a52e0f47678
Dominique Unruh <git.assessments.mkuftc@rwth.unruh.de>
2025-08-19 18:51:02 +0200

    Small cleanup.

--- problems/src/test/scala/assessments/DynexiteDefaultsTest.scala
+++ problems/src/test/scala/assessments/DynexiteDefaultsTest.scala
@@ -12,23 +12,21 @@ class DynexiteDefaultsTest extends AnyFunSuiteLike {
   }
 
   test("mathTry, valid parse") {
-    given GradingContext = gc(x = "12 + 3")
-    given commenter: Commenter = Commenter()
+    given context: GradingContext = gc(x = "12 + 3")
     val x = input("15")
     val result = x.mathTry("x")
     println(result)
     assert(result.toString == "plus(12, 3)")
-    assert(commenter.comments.isEmpty)
+    assert(context.comments.isEmpty)
   }
 
   test("mathTry, invalid parse") {
-    given GradingContext = gc(x = "12 + ")
-    given commenter: Commenter = Commenter()
+    given context: GradingContext = gc(x = "12 + ")
     val x = input("15")
     val result = x.mathTry("x")
     println(result)
     assert(result == StackMath.noAnswer)
-    assert(commenter.comments.length == 1)
-    assert(commenter.comments.head.toPlaintext == "Could not parse x, treating as no answer")
+    assert(context.comments.length == 1)
+    assert(context.comments.head.toPlaintext == "Could not parse x, treating as no answer")
   }
 }
--- problems/src/test/scala/tmp/Tmp.scala
+++ problems/src/test/scala/tmp/Tmp.scala
@@ -1,18 +1,12 @@
 package tmp
 
-import assessments.{Comment, Commenter, Points}
+import assessments.{Comment, Points}
 
 import scala.util.boundary
 import scala.util.boundary.{Label, break}
 
 object Tmp {
   def main(args: Array[String]): Unit = {
-    def g(f: Label[Points] ?=> Nothing): Unit = println(boundary(f))
-    g {
-      if (true)
-        break(5:Points)
-      else
-        break(6:Points)
-    }
+    
   }
 }
