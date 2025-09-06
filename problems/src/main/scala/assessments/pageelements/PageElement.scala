408478f6d567a96b50fdf4bdcdc72c2ef67717df
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2025-09-06 13:23:02 +0200

    Removed dead code

--- problems/src/main/scala/assessments/pageelements/PageElement.scala
+++ problems/src/main/scala/assessments/pageelements/PageElement.scala
@@ -25,40 +25,6 @@ trait PageElement extends Element { self =>
   val initialState: JsValue = JsObject(collection.Seq("content" -> JsString("")))
 }
 
-object PageElement {
-  private val eval = new Eval(mkReporter = None, nonCpOptions = Seq("-experimental"), classpath = Eval.currentClasspath, backingDir = None)
-
-  private val preamble =
-    """import scala.language.experimental.genericNumberLiterals
-      |import assessments.stack.{StackMath, StackUtils, StackParser}
-      |import assessments.{ElementName}
-      |import assessments.Preamble._
-      |""".stripMargin
-
-  def parsePageElement(spec: String): PageElement = {
-    val (nameRaw, code) = {
-      val index = spec.indexOf(':')
-      if (index == -1)
-        throw new SyntaxError(s"Could not parse tag `$spec`, missing `:`")
-      (spec.substring(0, index).trim, spec.substring(index + 1).trim)
-    }
-    val name = ElementName(nameRaw)
-    Using.resource(Context(name)) { context =>
-      val augmentedCode = preamble
-        + s"given assessments.Context = ${context.getterCode};\n"
-        + code
-//      Eval[PageElement](augmentedCode)
-//      println(classOf[PageElement].getName)
-//      println(augmentedCode)
-      val pageElement = eval.eval(augmentedCode, Some(classOf[PageElement].getName))
-        .getValue(getClass.getClassLoader)
-        .asInstanceOf[PageElement]
-      assert(pageElement.name == name)
-      pageElement
-    }
-  }
-}
-
 case class ElementAction(element: ElementName, data: JsValue)
 object ElementAction {
   def error(message: String): ElementAction =
