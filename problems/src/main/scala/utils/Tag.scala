8d858f4065a5777e8128873f0fcd2a21d9e9c707
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2025-09-07 10:35:53 +0200

    Tags: improved typing

--- problems/src/main/scala/utils/Tag.scala
+++ problems/src/main/scala/utils/Tag.scala
@@ -8,28 +8,27 @@ class Tag[Owner, Value](explicitName: String = "", val default: Value)(implicit 
 }
 
 object Tag {
-  class Tags[-Owner] private (private val map: Map[Tag[?, ?], Any]) extends AnyVal {
+  class Tags[-Owner] private (/** Contains `Tag[O,V] -> V` pairs, with `O >: Owner` */
+                               private val map: Map[Tag[?, ?], Any]) extends AnyVal {
     def get[Value](tag: Tag[?, Value]): Option[Value] =
       map.get(tag).asInstanceOf[Option[Value]]
     def getOrElse[Value](tag: Tag[?, Value], default: => Value): Value =
       get(tag).getOrElse(default)
     def apply[Value](tag: Tag[?, Value]): Value =
       get(tag).getOrElse(tag.default)
-    def +[Value](tagged: Tagged[?, Value]): Tags[Owner] = {
+    def +[O, Value](tagged: Tagged[O, Value]): Tags[Owner & O] = {
       assert(!map.contains(tagged.tag))
       new Tags(map + (tagged.tag.asInstanceOf[Tag[?, ?]] -> tagged.value))
     }
   }
-  case class Tagged[Owner, Value](tag: Tag[Owner, Value], val value: Value) {
-//    def +(tagged: Tagged[Owner, ?]): Tags[Owner] = Tags(this) + tagged
-  }
+  case class Tagged[Owner, Value](tag: Tag[Owner, Value], val value: Value)
 
   object Tagged {
     given [Owner, Value]: Conversion[Tagged[Owner, Value], Tags[Owner]] = Tags(_)
   }
 
   object Tags {
-    def apply[Owner](tags: Tagged[Owner, ?]*): Tags[Owner] = {
+    def apply[Owner](tags: Tagged[? >: Owner, ?]*): Tags[Owner] = {
       val builder = mkBuilder[Owner]
       for (tagged <- tags)
         builder += tagged
@@ -43,10 +42,10 @@ object Tag {
     class Builder[Owner] private[Tag] () {
       private val builder = Map.newBuilder[Tag[?, ?], Any]
 
-      def +=[Value](tagged: Tagged[Owner, Value]) : Unit =
+      def +=[O >: Owner, Value](tagged: Tagged[O, Value]) : Unit =
         this += (tagged.tag, tagged.value)
 
-      def +=[Value](tagValue: (Tag[Owner, Value], Value)) : Unit =
+      def +=[O >: Owner, Value](tagValue: (Tag[O, Value], Value)) : Unit =
         builder += tagValue
 
       def result(): Tags[Owner] = new Tags(builder.result())
