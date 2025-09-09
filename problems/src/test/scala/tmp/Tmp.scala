c59efafa7c0667015302742b35b582c1163e823f
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2025-09-09 15:32:32 +0200

    Dynexite JSON parser: Consolidate ..._sub_?_? fields into single matrix description

--- problems/src/test/scala/tmp/Tmp.scala
+++ problems/src/test/scala/tmp/Tmp.scala
@@ -1,15 +1,17 @@
 package tmp
 
-import assessments.DynexiteDefaults.elementName
+import assessments.DynexiteDefaults.{InputElementMethods, elementName, math}
 import assessments.pageelements.InputElement
 import assessments.stack.{StackMath, StackParser, SympyExpr}
 import assessments.stack.StackMath.Ops
 import assessments.stack.StackParser.{maximaToStackMath, parseArray}
 import assessments.{DynexiteDefaults, ElementName, Exam, Html, MathContext, SyntaxError}
 import externalsystems.MoodleStack
+import externalsystems.MoodleStack.InputType.matrix
 import externalsystems.MoodleStack.{Question, Quiz, inputElementToMoodle, moodleAllowWords, moodleInputType, moodleQuestionVariables}
 import me.shadaj.scalapy.py
 import ujson.{Arr, Bool, Null, Num, Obj, Str, Value}
+import utils.Tag.Tags
 import utils.{Docker, Python}
 
 import java.io.StringReader
@@ -36,27 +38,12 @@ object Tmp {
   class Context
 
   def main(args: Array[String]): Unit = {
-    class Player {
-      private var _points: Int = 0
-
-      def points(using ctx: Context): Int = _points
-
-      def points_=(value: Int)(using ctx: Context): Unit = {
-        _points = math.max(0, value) // Ensure points never go negative
-      }
-
-      def points_+=(value: Int)(using ctx: Context): Unit = {
-        _points = math.max(0, value) // Ensure points never go negative
-      }
-    }
-
-    given Context()
-    val player = new Player
-    player.points = 100 // Works
-    player.points = player.points + 50
-//    player.points += 50 // Works - becomes player.points = player.points + 50
-//    player.points -= 25 // Works
-//    player.points *= 2 // Works
-    println(player.points) // 250
+    val name = "ans2_sub_2_0"
+    println(!name.matches(".*_sub_[0-9]+_[0-9]+"))
+    
+    val input = DynexiteDefaults.input("transpose(matrix([0,0,i,0]))", Tags(moodleInputType := matrix))
+    val str = "transpose(matrix([0,0,i,0]))"
+    val parsed = str.math(input)
+    println(parsed)
   }
 }
