2ff36a4234f62861d13007a93b028c81c8850975
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2026-04-09 13:37:56 +0200

    Webapp: Problems not sorted alphabetically, numbered list

--- webapp/app/controllers/AssessmentController.scala
+++ webapp/app/controllers/AssessmentController.scala
@@ -65,12 +65,12 @@ class AssessmentController @Inject()(val controllerComponents: ControllerCompone
     val exam = getExam(examName)
     val html = StringBuilder()
     html ++= s"<h1>Exam ${exam.name}</h1>\n"
-    html ++= "<ul>\n"
-    for (problem <- exam.problems.sortBy(_.name))
+    html ++= "<ol>\n"
+    for (problem <- exam.problems)
       val escapedName = StringEscapeUtils.escapeHtml4(problem.name)
       val link = routes.AssessmentController.assessment(examName, problem.name).url
       html ++= s"""  <li><a href="$link">$escapedName</a> (${problem.getClass.getSimpleName.stripSuffix("$")})</li>\n"""
-    html ++= "</ul>\n"
+    html ++= "</ol>\n"
     Ok(Html(html.result()))
   }
 
