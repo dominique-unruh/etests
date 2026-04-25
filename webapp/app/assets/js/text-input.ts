a7a6248b3ad26ff990528ae8ed9b761450ee3e87
Dominique Unruh <git.etests.qiccmr@rwth.unruh.de>
2026-04-25 04:52:19 +0300

    text-input: Obey columns/rows attributes

--- webapp/app/assets/js/text-input.ts
+++ webapp/app/assets/js/text-input.ts
@@ -1,11 +1,20 @@
 import {InteractiveElement} from "./interactive-element.js";
 
 export class TextInput extends InteractiveElement<string, null> {
-    private inputField: HTMLInputElement;
+    private inputField: HTMLInputElement | HTMLTextAreaElement;
 
     constructor() {
         super("");
-        this.inputField = document.createElement("input")
+        const rows = this.getAttribute("rows")!=null ? parseInt(this.getAttribute("rows")) : 1
+        const columns = this.getAttribute("columns")!=null ? parseInt(this.getAttribute("columns")) : 15
+        if (rows == 1) {
+            this.inputField = document.createElement("input")
+            this.inputField.setAttribute("size", columns.toString());
+        } else {
+            this.inputField = document.createElement("textarea")
+            this.inputField.setAttribute("cols", columns.toString());
+            this.inputField.setAttribute("rows", rows.toString());
+        }
         this.appendChild(this.inputField);
         this.inputField.addEventListener("input", _event => {
             this.updateContent(this.inputField.value)
