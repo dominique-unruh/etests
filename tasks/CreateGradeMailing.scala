ebf60bd2199d2a91ee3cc060bd381a8ff7c40dee
Dominique Unruh <noemail@example.com>
2026-08-26 14:49:58 +0200

    CreateGradeMailing: Added message template in comment.

--- tasks/CreateGradeMailing.scala
+++ tasks/CreateGradeMailing.scala
@@ -25,7 +25,6 @@ object CreateGradeMailing extends Task {
   val rows = for (row <- examResults.rows) yield {
     assert(row.grade.nonEmpty)
     val grade = f"${row.grade.toDouble}%.1f"
-    println(grade)
     val regno = row.student
     val scieboLink = Sciebo.getPublicReadLink(scieboRoot, scieboReportDir.resolve(regno))
     val name = allStudentsSheet.lookup("registration", regno).name
@@ -44,10 +43,10 @@ object CreateGradeMailing extends Task {
 
 /*
 
-To: {{name}} {{email}}
+To: {{name}} <{{email}}>
 Subject: Grade for EXAM-NAME
 
-Dear {{name}} ({{regno}}),
+Dear {{name}} ({{registration}}),
 
 in the EXAM-NAME, you received the grade: {{grade}}
 
@@ -64,3 +63,57 @@ Best wishes,
 Dominique.
 
 */
+
+
+/*
+
+Moodle message:
+
+
+
+
+Dear all,
+
+the exam has been graded. If you participated, you should have gotten an email with the grade.
+If you did not get one, please contact me.
+
+The email also contained a link to a personal Sciebo folder with detailed grading information. The password for the folder is "password".
+
+This Sciebo folder contains:
+
+    grading.pdf: Your exam, with your grade and detailed information about the grading of each problem.
+    grading.html: Same as HTML.
+    dynexite.pdf: Your exam as downloaded directly from Dynexite. (It shows how Dynexite processed your input in case you suspect something broke into the translation into our software.)
+
+The grade distribution (before the exam inspection, including only students who showed up) was:
+
+TODO: Copy here.
+
+
+About the exam inspection:
+
+Exam inspection has two phases (each of them optional):
+
+Email phase:
+
+If you find problems in the correction, you can send them to me via email. The email must be of the following form:
+
+* Subject must begin with POST-QUANTUM-CRYPTO EXAM INSPECTION
+* The email must start with your name and registration number.
+* Any comments must be preceded by name of the exam question it refers to. (Question names can be seen in the grading.pdf.)
+
+There will be no response. There will be only an email saying "processed" so that you know it has been processed,
+but not explanation how or why and whether your request was accepted.
+However, the grading.pdf will be updated right away, so you should recheck there to see if something has changed.
+
+Important: Before writing your email, you should recheck the grading.pdf before coming thought because it may be updated with corrections due to problems identified by other students.
+
+In-person phase:
+
+You can come to the exam inspection in the QIS chair (Monday, August 31, 10:00-11:30) whether or not you did send something by email.
+You should recheck the grading.pdf before coming thought because it may be updated with corrections due to problems identified by other students.
+
+Best wishes,
+Dominique.
+
+ */
